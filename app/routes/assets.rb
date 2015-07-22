cf605fe9c27a10d5ed6a815361465b644977947d
sloo <sloo@rti.org>
2015-07-22 15:21:44 -0400

    Asset pipeline issue, loading bootstrap

--- app/routes/assets.rb
+++ app/routes/assets.rb
@@ -1,16 +1,10 @@
 module Cms
   module Routes
     class Assets < Base
-      configure do
-                    set :assets, assets = Sprockets::Environment.new(settings.root)
-                    assets.append_path('app/assets/javascripts')
-                    assets.append_path('app/assets/stylesheets')
+      get '/assets/*' do
+        env['PATH_INFO'].sub!(%r{^/assets}, '')
+        settings.assets.call(env)
       end
-            
-      # get '/assets/*' do
-#         env['PATH_INFO'].sub!(%r{^/assets}, '')
-#         settings.assets.call(env)
-#       end
     end
   end
 end
