2fd6bfd6b7efe1cfc2fd92cc24f9b5c8a7833297
sloo <sloo@rti.org>
2015-07-22 08:31:52 -0400

    Redefine helper files

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,8 +1,5 @@
 module Cms
   module Helpers
-    def fullname(person)
-      person.first_name+" "+person.last_name
-    end 
-    #require 'app/helpers/people_helper'
+    require 'app/helpers/people_helper'
   end
 end
--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,5 +1,4 @@
-module PeopleHelper
+
   def fullname(person)
     person.first_name+" "+person.last_name
   end  
-end
