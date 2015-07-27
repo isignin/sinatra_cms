bb94408dbbe6e3d01cd2818cf38195bd47bf62a7
sloo <sloo@rti.org>
2015-07-27 16:18:10 -0400

    Add fields to people table

--- config/database.rb
+++ config/database.rb
@@ -22,9 +22,22 @@ unless DB.table_exists?(:people)
     Integer :case_id
     String :first_name
     String :last_name
+    String :middle_name
+    String :nickname
     String :gender
     String :birthdate
     String :parent_name
+    String :date_detected
+    String :form_completed_by
+    String :info_transmitted_by 
+    String :notified_by
+    String :notification_group
+    String :relationship_to_patient
+    String :head_of_household
+    String :village_district
+    String :country
+    String :nationality
+    String :ethnic_group
    end
 end
 
--- db/migrations/CreatePeople.rb
+++ db/migrations/CreatePeople.rb
@@ -5,9 +5,22 @@ Sequel.migration do
       Integer :case_id
       String :first_name
       String :last_name
+      String :middle_name
+      String :nickname
       String :gender
       String :birthdate
-      String :parent_name  
+      String :parent_name 
+      String :date_detected
+      String :form_completed_by
+      String :info_transmitted_by 
+      String :notified_by
+      String :notification_group
+      String :relationship_to_patient
+      String :head_of_household
+      String :village_district
+      String :country
+      String :nationality
+      String :ethnic_group
     end
   end
 
