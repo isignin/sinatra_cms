b6c5491c1d3c11ed2acbf0d55063d0a89f6514b7
Steve Loo <isignin@gmail.com>
2015-07-29 20:36:34 -0400

    User views updated

--- app.rb
+++ app.rb
@@ -24,7 +24,6 @@ require 'rake'
 require 'logger'
 
 Dir['lib/**/*.rb'].sort.each { |file| require file }
-#Dir['app/helpers/**/*.rb'].sort.each { |file| require file }
 
 require 'config/database'
 require 'app/extensions'
@@ -41,7 +40,7 @@ module Cms
     configure do
       register Sinatra::Partial
 
-      disable :method_override, :static
+      disable :static
       enable :session, :logging
       
       file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
