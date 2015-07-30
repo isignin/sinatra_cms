0ae9ea194214d5268e52b09e4479213be9c73b1c
Steve Loo <isignin@gmail.com>
2015-07-30 17:44:03 -0400

    Reconfigure for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
  
 require 'rack/test'
-require 'ruby-debug'
-require 'spec'
-require 'spec/autorun'
-require 'spec/interop/test'
+require 'rspec'
  
-# set test environment
-set :environment, :test
-set :run, false
-set :raise_errors, true
-set :logging, false
+ENV['RACK_ENV'] = 'test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() described_class end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }
